2ecf9f4bd78c015d2370023933e0b3d48b55263b
Krjamwat <thisisforstudy191@gmail.com>
2025-09-09 16:19:54 +0700

    update

--- cell03/ex00/backgroound.js
+++ cell03/ex00/backgroound.js
@@ -1,7 +1,7 @@
 function random_color(){
     var hex , color ;
     hex = '0123456789ABCDEF';
-    color = '#'
+    color = '#';
     console.log(Math.random()*16);
     for(let i = 0 ;i < 6 ; i++) color += hex[Math.floor(Math.random() * 16)];
     return color ;
